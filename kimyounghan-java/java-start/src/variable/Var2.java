e7325d5bc4cffd4ac83c61e67458b9f9b42e96d5
silent110 <114848918+silent110@users.noreply.github.com>
2024-03-07 15:20:54 +0900

    Add files via upload
    자바 연습장
    
    오후 3시까지 공부한 내용

---
+++ kimyounghan-java/java-start/src/variable/Var2.java
@@ -0,0 +1,22 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args){
+        int a; // -> 숫자를 담을 수 있는 통 int -> integer -> 정수 변수 선언
+        a = 10; // 만들어진 그릇에다가(int a) '='은 대입이라고 한다 '='을 하면 10을 a라는 그릇에 넣습니다 a라는 그릇에 10이 들어간다  변수 초기화
+//        20을 세번 출력을 하라
+
+
+        System.out.println(a);
+//  출력을 하는데 a에 담겨 있는 걸 읽어서 출력을 한다
+        System.out.println(a);
+        System.out.println(a);
+//        값을 읽는다고 해서 값이 사라지지 않는다 확인
+
+
+
+    }
+
+
+
+}

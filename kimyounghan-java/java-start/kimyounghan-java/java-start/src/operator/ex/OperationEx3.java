d31733afdcd2d6b2ed625272e1cf3120c5c08729
silent110 <114848918+silent110@users.noreply.github.com>
2024-03-31 20:59:24 +0900

    Delete kimyounghan-java/java-start directory

--- kimyounghan-java/java-start/kimyounghan-java/java-start/src/operator/ex/OperationEx3.java
+++
@@ -1,9 +0,0 @@
-package operator.ex;
-
-public class OperationEx3 {
-    public static void main(String[] args) {
-        int score = 90;
-        boolean test = (score > 80 && score <= 100);
-        System.out.println(test);
-    }
-}
